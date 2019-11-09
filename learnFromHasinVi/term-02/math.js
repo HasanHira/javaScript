498069e690ccdfe9ac9bacfdf31555190e172ff9
hasanhira <qubitgraphix@gmail.com>
2019-11-09 21:26:28 +0600

    Completed term 01 and a new task term 02

---
+++ learnFromHasinVi/term-02/math.js
@@ -0,0 +1,28 @@
+// Egg cost calculator;
+
+var eggPrice = 8;
+var costOfEgg = 12;
+var totalCostOfEggs = eggPrice * costOfEgg;
+
+console.log("Total",costOfEgg,"eggs price is " + totalCostOfEggs + " taka.");
+
+
+// Circle math calculator // circle = pi * r squared
+
+var radius = 3;
+const pi = 3.14159;
+
+var areaOfCircle = pi * ( radius * radius );
+
+console.log( radius + " inches radius circle is", areaOfCircle,'square inches.');
+
+// BDT to USD calculator
+
+var rateOfUsd = 84.71;
+var haveUsd = 140;
+var haveBdt = 5000;
+var bdtToUsd = haveUsd * rateOfUsd;
+var convToUsd = haveBdt / rateOfUsd;
+
+console.log(bdtToUsd + " taka");
+console.log("$", convToUsd);
