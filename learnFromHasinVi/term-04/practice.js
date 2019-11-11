cb9aaf079dc87b03fbaf991da2d6db15a67d2e1a
hasanhira <qubitgraphix@gmail.com>
2019-11-12 00:19:09 +0600

    Modify index.html in term-04.

---
+++ learnFromHasinVi/term-04/practice.js
@@ -0,0 +1,12 @@
+var arrValue = [345, 32, 356];
+
+if (arrValue[0]>arrValue[1] || arrValue[0]>arrValue[2]){
+	console.log(arrValue[0]);
+	console.log(arrValue[0] + " is greater than all value.");
+} else if(arrValue[1]>arrValue[0] || arrValue[1]>arrValue[2]){
+	console.log(arrValue[1]);
+	console.log(arrValue[1] + " is greater than all value.");
+} else {
+	console.log(arrValue[2]);
+	console.log(arrValue[2] + " is greater than all value.");
+}
