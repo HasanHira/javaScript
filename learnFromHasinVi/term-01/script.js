d19850e3c97080c944bed7e88e14411f454c75af
hasanhira <qubitgraphix@gmail.com>
2019-11-12 00:15:38 +0600

    Add a new practice.js in term-04

---
+++ learnFromHasinVi/term-01/script.js
@@ -0,0 +1,30 @@
+var myName = "Hasan Hira";
+var myAge;
+
+console.log("My name is", myName);
+
+myAge = 39;
+
+console.log("My name is", myName, "and my age is " + myAge + " years.");
+
+// So now we learn about CONSTANT
+
+//var pi = 3.14159; //this is variable we can set it various times.
+const pi = 3.14159 // Now this pi is declear to constant and it will be not change;
+//pi = 22; // so why, it will give us an error.
+
+console.log("Value of pi", pi);
+
+// example another
+const myFullName = "Hasanuzzaman Hira"
+
+console.log("My full name is " + myFullName);
+
+// Comments 
+
+// This is single line comment.
+
+/*
+This is multi line comment.
+It will bla bal .....
+*/
