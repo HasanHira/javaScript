7b0080114bcb318bd136365e1cc74793ec3da764
hasanhira <qubitgraphix@gmail.com>
2019-11-12 17:09:44 +0600

    solved the previous errors

--- learnFromHasinVi/term-04/practice.js
+++ learnFromHasinVi/term-04/practice.js
@@ -1,12 +1,14 @@
-var arrValue = [345, 32, 356];
+// Compare three numbers which one is big 420, 654, 18;
 
-if (arrValue[0]>arrValue[1] || arrValue[0]>arrValue[2]){
+var arrValue = [420, 654, 18];
+
+if (arrValue[0]>arrValue[1] && arrValue[0]>arrValue[2]){
 	console.log(arrValue[0]);
 	console.log(arrValue[0] + " is greater than all value.");
-} else if(arrValue[1]>arrValue[0] || arrValue[1]>arrValue[2]){
+} else if(arrValue[1]>arrValue[0] && arrValue[1]>arrValue[2]){
 	console.log(arrValue[1]);
 	console.log(arrValue[1] + " is greater than all value.");
-} else {
+} else if(arrValue[2]>arrValue[0] && arrValue[2]>arrValue[1]){
 	console.log(arrValue[2]);
 	console.log(arrValue[2] + " is greater than all value.");
 }
