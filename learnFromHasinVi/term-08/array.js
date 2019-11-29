10538d16a8ec7bf7ab7356b39e5d66f5af30967d
hasanhira <qubitgraphix@gmail.com>
2019-11-30 01:58:21 +0600

    start learning javascript array in a new task term 08

---
+++ learnFromHasinVi/term-08/array.js
@@ -0,0 +1,68 @@
+// About array
+/*
+var foods = ["rice", "bread", "noodles", "biscuits", "cake"];
+
+console.log(foods,'\n');
+
+console.log(foods[3]); // array starts with 0 index or offset;
+
+console.log(foods[5]);
+
+foods[5] = "burger"; // for inseart an item in array.
+
+console.log(foods[5]);
+
+console.log(foods.length); // to know how many items in the array.
+*/
+
+/*
+// 1. to know last items;
+var foods = ["rice", "bread", "noodles", "biscuits", "cake", "burger"];
+
+
+var lastItem = foods.length - 1;
+
+console.log(foods[lastItem]);
+
+// 2. to add an item;
+
+var addItem = foods.length;
+
+foods[addItem] = "dounut";
+
+var lastItem = foods.length - 1;
+console.log(foods[lastItem]);
+console.log(foods);
+*/
+
+// 1. Add and remove item;
+var foods = ["rice", "bread", "noodles", "biscuits"];
+console.log(foods);
+
+foods.push("patise"); // to add item in last offset
+
+console.log(foods);
+
+foods.unshift("danis"); // to add item in first offset
+
+console.log(foods);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
